bb83c035e5697e611d31ef3b3cfecb10322b932c
gamadv <lealdev.py@gmail.com>
2022-09-26 21:25:39 -0300

    feat: Header component created

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,15 +4,22 @@ import { Container } from "./styles";
 export function Header() {
   return (
     <Container>
-      <Image
-        src="/images/logo-icon.svg"
-        alt="Logo"
-        width="114px"
-        height="27px"
-      ></Image>
-      <button>
-        <Image src="/images/cart.svg" alt="Logo" width="34px" height="34px"></Image>
-      </button>
+      <div>
+        <Image
+          src="/images/logo-icon.svg"
+          alt="Logo"
+          width="114px"
+          height="27px"
+        ></Image>
+        <button>
+          <Image
+            src="/images/cart.svg"
+            alt="Logo"
+            width="34px"
+            height="34px"
+          ></Image>
+        </button>
+      </div>
     </Container>
   );
 }
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,13 +1,20 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-
-  padding: 1rem;
-
-  height: auto;
-  max-height: 64px;
   background: linear-gradient(165.98deg, #3135c6 14.08%, #00aaff 89.86%);
+  div {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    padding: 1rem;
+
+    height: auto;
+    max-height: 64px;
+    
+    @media screen and (min-width: 1024px) {
+      max-width: 1200px;
+      margin-inline: auto;
+    }
+  }
 `;
